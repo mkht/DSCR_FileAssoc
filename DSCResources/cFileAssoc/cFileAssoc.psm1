2921164cbf16bd405d88b3a6447761db0cdc8a33
mkht <mkht@users.noreply.github.com>
2017-07-23 17:05:43 +0900

    Code Format

--- DSCResources/cFileAssoc/cFileAssoc.psm1
+++ DSCResources/cFileAssoc/cFileAssoc.psm1
@@ -2,13 +2,12 @@
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////
-function Get-TargetResource
-{
+function Get-TargetResource {
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [ValidateSet("Present","Absent")]
+        [ValidateSet("Present", "Absent")]
         [string]
         $Ensure = 'Present',
 
@@ -30,21 +29,21 @@ function Get-TargetResource
     )
 
     $GetRes = @{
-        Ensure = $Ensure
+        Ensure    = $Ensure
         Extension = $Extension
-        Command = ''
-        FileType = ''
-        Icon = ''
+        Command   = ''
+        FileType  = ''
+        Icon      = ''
     }
 
     $GetAssoc = Get-FileAssoc -Extension $Extension
     $GetRes.FileType = $GetAssoc.FileType
     $GetRes.Command = $GetAssoc.Command
     $GetRes.Icon = $GetAssoc.Icon
-    if($GetRes.Command -and $GetRes.FileType){
+    if ($GetRes.Command -and $GetRes.FileType) {
         $GetRes.Ensure = 'Present'
     }
-    else{
+    else {
         $GetRes.Ensure = 'Absent'
     }
     $GetRes
@@ -52,13 +51,12 @@ function Get-TargetResource
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////
-function Test-TargetResource
-{
+function Test-TargetResource {
     [CmdletBinding()]
     [OutputType([System.Boolean])]
     param
     (
-        [ValidateSet("Present","Absent")]
+        [ValidateSet("Present", "Absent")]
         [string]
         $Ensure = 'Present',
 
@@ -82,20 +80,24 @@ function Test-TargetResource
     $Ret = $true
 
     $CurrentState = Get-TargetResource -Ensure $Ensure -Extension $Extension
-    if($Ensure -ne $CurrentState.Ensure){   # Not match Ensure state
+    if ($Ensure -ne $CurrentState.Ensure) {
+        # Not match Ensure state
         Write-Verbose ('Not match Ensure state. your desired "{0}" but current "{1}"' -f $Ensure, $CurrentState.Ensure)
         $Ret = $Ret -and $false
     }
-    if($Ensure -eq 'Present'){
-        if($Command -ne $CurrentState.Command){ # Not match associated command
+    if ($Ensure -eq 'Present') {
+        if ($Command -ne $CurrentState.Command) {
+            # Not match associated command
             Write-Verbose ('Command attr is not match')
             $Ret = $Ret -and $false
         }
-        if($PSBoundParameters.FileType -and ($FileType -ne $CurrentState.FileType)){ # Not match FileType (optional)
+        if ($PSBoundParameters.FileType -and ($FileType -ne $CurrentState.FileType)) {
+            # Not match FileType (optional)
             Write-Verbose ('FileType attr is not match')
             $Ret = $Ret -and $false
         }
-        if($PSBoundParameters.Icon -and ($Icon -ne $CurrentState.Icon)){ # Not match Icon (optional)
+        if ($PSBoundParameters.Icon -and ($Icon -ne $CurrentState.Icon)) {
+            # Not match Icon (optional)
             Write-Verbose ('Icon attr is not match')
             $Ret = $Ret -and $false
         }
@@ -106,12 +108,11 @@ function Test-TargetResource
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////
-function Set-TargetResource
-{
+function Set-TargetResource {
     [CmdletBinding()]
     param
     (
-        [ValidateSet("Present","Absent")]
+        [ValidateSet("Present", "Absent")]
         [string]
         $Ensure = 'Present',
 
@@ -132,33 +133,35 @@ function Set-TargetResource
         $Icon
     )
 
-    if($Ensure -eq 'Absent'){   #関連付け削除
+    if ($Ensure -eq 'Absent') {
+        #関連付け削除
         $Res = @{
             Extension = $Extension
-            FileType = ''
-            Command = ''
-            Icon = ''
+            FileType  = ''
+            Command   = ''
+            Icon      = ''
         }
     }
-    elseif($Ensure -eq 'Present'){   #関連付け登録
+    elseif ($Ensure -eq 'Present') {
+        #関連付け登録
         $Res = Get-FileAssoc -Extension $Extension
         $Res.Command = $Command
-        if($PSBoundParameters.FileType){    #FileType指定あり -> 指定されたFileTypeを使う
+        if ($PSBoundParameters.FileType) {
+            #FileType指定あり -> 指定されたFileTypeを使う
             $Res.FileType = $FileType
         }
-        elseif(-not $Res.FileType){ #FileType未設定 & FileType指定なし -> 拡張子(ドット無)+file を使う eg).txt -> txtfile
+        elseif (-not $Res.FileType) {
+            #FileType未設定 & FileType指定なし -> 拡張子(ドット無)+file を使う eg).txt -> txtfile
             $Res.FileType = ('{0}fiie' -f $Extension.TrimStart('.'))
         }
         # アイコン指定あり
-        if($PSBoundParameters.Icon){
+        if ($PSBoundParameters.Icon) {
             $Res.Icon = $Icon
         }
     }
 
     Set-FileAssoc @Res
 } # end of Set-TargetResource
-
-
 
 # ////////////////////////////////////////////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////////////////////////////////
@@ -172,38 +175,36 @@ function Get-FileAssoc {
 
     $Ret = @{
         Extension = $Extension
-        FileType = ''
-        Command = ''
-        Icon = ''
+        FileType  = ''
+        Command   = ''
+        Icon      = ''
     }
 
     # ユーザ固有の関連付けがある場合はそちらを取得
     $UserChoicePath = ("HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\{0}\UserChoice" -f $Extension)
-    if((Test-Path $UserChoicePath) -and (Get-ItemProperty $UserChoicePath).ProgId){
+    if ((Test-Path $UserChoicePath) -and (Get-ItemProperty $UserChoicePath).ProgId) {
         $Ret.FileType = (Get-ItemProperty $UserChoicePath).ProgId
     }
     # なければシステム全体の関連付けを取得
-    else{
+    else {
         $GetFileType = & cmd.exe /c ("assoc {0} 2>null" -f $Extension)
-        foreach($Line in $GetFileType)
-        {
-            if($Line -match '='){
+        foreach ($Line in $GetFileType) {
+            if ($Line -match '=') {
                 $Ret.FileType = $Line.Split("=")[1].Trim()
             }
         }
     }
 
-    if($Ret.FileType){
+    if ($Ret.FileType) {
         $GetCommand = & cmd.exe /c ("ftype {0} 2>null" -f $Ret.FileType)
-        foreach($Line in $GetCommand)
-        {
-            if($Line -match '='){
+        foreach ($Line in $GetCommand) {
+            if ($Line -match '=') {
                 $Ret.Command = $Line.Split("=")[1].Trim()
             }
         }
 
         $RegKey = [Registry]::LocalMachine.OpenSubKey(("SOFTWARE\Classes\{0}\DefaultIcon" -f $Ret.FileType))
-        if($RegKey){
+        if ($RegKey) {
             $Ret.Icon = $RegKey.GetValue($null, $null, [RegistryValueOptions]::DoNotExpandEnvironmentNames)
             $RegKey.Close()
         }
@@ -235,7 +236,7 @@ function Set-FileAssoc {
 
     # ユーザ固有の関連付けは削除する (アクセス権の問題でトリッキーな消し方をする必要がある)
     $UserChoicePath = ("Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\{0}\UserChoice" -f $Extension)
-    if($RegKey = [Registry]::CurrentUser.OpenSubKey($UserChoicePath, [RegistryKeyPermissionCheck]::ReadWriteSubTree, [System.Security.AccessControl.RegistryRights]::ChangePermissions)){
+    if ($RegKey = [Registry]::CurrentUser.OpenSubKey($UserChoicePath, [RegistryKeyPermissionCheck]::ReadWriteSubTree, [System.Security.AccessControl.RegistryRights]::ChangePermissions)) {
         $Acl = $RegKey.GetAccessControl()
         $Acl.Access | ? {$_.AccessControlType -eq 'Deny'} | % { [void]$Acl.RemoveAccessRule($_) }
         $RegKey.SetAccessControl($Acl)
@@ -245,16 +246,16 @@ function Set-FileAssoc {
 
     # 拡張子とファイルタイプの紐付け
     $SetFileType = & cmd.exe /c ("assoc {0}={1}" -f $Extension, $FileType)
-    if($FileType){
+    if ($FileType) {
         # ファイルタイプと実行コマンドの紐付け
-        $SetCommand = & cmd.exe /c ("ftype {0}={1} 2>null" -f $FileType, $Command.Replace('%','^%'))    # Powershellではなくコマンドラインの動作仕様に引きずられるので%を^%にエスケープする必要あり
+        $SetCommand = & cmd.exe /c ("ftype {0}={1} 2>null" -f $FileType, $Command.Replace('%', '^%'))    # Powershellではなくコマンドラインの動作仕様に引きずられるので%を^%にエスケープする必要あり
         # ファイルアイコンの設定
         $Key = ("HKLM:\SOFTWARE\Classes\{0}\DefaultIcon" -f $FileType)
-        if(-not (Test-Path $Key)){
+        if (-not (Test-Path $Key)) {
             New-Item -Path $Key -Force | Out-Null
         }
         $RegKey = [Registry]::LocalMachine.OpenSubKey(("SOFTWARE\Classes\{0}\DefaultIcon" -f $FileType), $true)
-        if($RegKey){
+        if ($RegKey) {
             $RegKey.SetValue("", $Icon, [RegistryValueKind]::ExpandString)
             $RegKey.Close()
         }
